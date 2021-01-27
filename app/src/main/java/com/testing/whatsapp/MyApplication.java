4e1d9b113cb187ecbb6385873cba8689fce202f5
Mohammadreza khahani <mr.khahani@gmail.com>
2021-01-28 00:41:51 +0330

    firebase InAppMessage feature added.

--- app/src/main/java/com/testing/whatsapp/MyApplication.java
+++ app/src/main/java/com/testing/whatsapp/MyApplication.java
@@ -2,5 +2,12 @@ package com.testing.whatsapp;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.testing.whatsapp.firebase.InAppMessage;
+
 public class MyApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        new InAppMessage().run();
+    }
 }
--- app/src/main/java/com/testing/whatsapp/firebase/InAppMessage.java
+++ app/src/main/java/com/testing/whatsapp/firebase/InAppMessage.java
@@ -1,9 +1,20 @@
 package com.testing.whatsapp.firebase;
 
+import android.util.Log;
+
+import com.google.firebase.BuildConfig;
+import com.google.firebase.installations.FirebaseInstallations;
+
 public class InAppMessage implements Runnable {
+
+    private void getId() {
+        FirebaseInstallations.getInstance().getId().addOnCompleteListener(task -> Log.d("Khahani", task.getResult()));
+    }
 
     @Override
     public void run() {
-
+        //khahani: determine a better condition after build process optimized.
+        if (BuildConfig.DEBUG)
+            getId();
     }
 }
