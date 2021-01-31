2b9426e9de34452a2ba64fde87765bafe0bcfdd7
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-01 00:21:53 +0330

    module is ready for work on dev

--- app/src/main/java/com/testing/whatsapp/creator/firebase/AnalyticsCreator.java
+++ app/src/main/java/com/testing/whatsapp/creator/firebase/AnalyticsCreator.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import com.khahani.usecase_firebase.Creator;
 import com.khahani.usecase_firebase.analytic.Analytics;
-import com.testing.firebase.AnalyticsImpl;
+import com.khahani.usecase_firebase.analytic.NullAnalytics;
 
 public class AnalyticsCreator extends Creator<Analytics> {
     private final Context context;
@@ -15,7 +15,7 @@ public class AnalyticsCreator extends Creator<Analytics> {
 
     @Override
     public Analytics factoryMethod() {
-//            return new NullAnalytics(context);
-            return new AnalyticsImpl(context);
+        return new NullAnalytics(context);
+//            return new AnalyticsImpl(context);
     }
 }
--- app/src/main/java/com/testing/whatsapp/creator/firebase/RemoteConfigCreator.java
+++ app/src/main/java/com/testing/whatsapp/creator/firebase/RemoteConfigCreator.java
@@ -1,13 +1,13 @@
 package com.testing.whatsapp.creator.firebase;
 
 import com.khahani.usecase_firebase.Creator;
+import com.khahani.usecase_firebase.NullRemoteConfig;
 import com.khahani.usecase_firebase.RemoteConfig;
-import com.testing.firebase.RemoteConfigImpl;
 
 public class RemoteConfigCreator extends Creator<RemoteConfig> {
     @Override
     public RemoteConfig factoryMethod() {
-//        return new NullRemoteConfig();
-        return new RemoteConfigImpl();
+        return new NullRemoteConfig();
+//        return new RemoteConfigImpl();
     }
 }
