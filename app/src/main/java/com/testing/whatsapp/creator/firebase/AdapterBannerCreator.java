93c36a42884dd73231bdfd41fac179a2bf4224ec
Mohammadreza khahani <mr.khahani@gmail.com>
2021-01-31 21:53:39 +0330

    create real firebase banner when INCLUDE_FIREBASE is true.

--- app/src/main/java/com/testing/whatsapp/creator/firebase/AdapterBannerCreator.java
+++ app/src/main/java/com/testing/whatsapp/creator/firebase/AdapterBannerCreator.java
@@ -1,6 +1,6 @@
 package com.testing.whatsapp.creator.firebase;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.View;
 
 import androidx.annotation.StringRes;
@@ -8,17 +8,19 @@ import androidx.annotation.StringRes;
 import com.khahani.usecase_firebase.Creator;
 import com.khahani.usecase_firebase.admob.AdapterBanner;
 import com.khahani.usecase_firebase.admob.NullAdapterBanner;
+import com.testing.firebase.admob.AdapterBannerImpl;
+import com.testing.whatsapp.BuildConfig;
 
 public class AdapterBannerCreator extends Creator<AdapterBanner> {
 
-    private final Context context;
+    private final Activity context;
     private final View layout;
     @StringRes
     private final int bannerContainerId;
     private final String realBannerId;
 
-    public AdapterBannerCreator(Context context, View layout, int bannerContainerId, String realBannerId) {
-        this.context = context;
+    public AdapterBannerCreator(Activity activity, View layout, int bannerContainerId, String realBannerId) {
+        this.context = activity;
         this.layout = layout;
         this.bannerContainerId = bannerContainerId;
         this.realBannerId = realBannerId;
@@ -26,7 +28,10 @@ public class AdapterBannerCreator extends Creator<AdapterBanner> {
 
     @Override
     public AdapterBanner factoryMethod() {
-        return new NullAdapterBanner();
-        // new AdapterBanner(context, layout, bannerContainerId, realBannerId);
+        if (!BuildConfig.INCLUDE_FIREBASE) {
+            return new NullAdapterBanner();
+        } else {
+            return new AdapterBannerImpl(context, layout, bannerContainerId, realBannerId);
+        }
     }
 }
