dec8287e571d75299758e889c148211d98f8de2c
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-01 22:33:02 +0330

    detect group and sender from a wrong messages.

--- app/src/test/java/com/testing/whatsapp/GroupTest.java
+++ app/src/test/java/com/testing/whatsapp/GroupTest.java
@@ -5,26 +5,57 @@ import org.junit.Test;
 
 public class GroupTest {
 
+    private final String[] titles = new String[]{
+            "E @ Thebook",
+            "حسین داداشی @ Distance",
+    };
+
+    private final String[] groups = new String[]{
+            "Thebook",
+            "Distance"
+    };
+
+    private final String[] senders = new String[]{
+            "E",
+            "حسین داداشی"
+    };
+    private String title;
+
     @Test
-    public void when_a_sender_contain_at_sign_then_is_group() {
-
-        String[] senders = new String[]{
-                "E @ Thebook",
-                "حسین داداشی @ Distance",
-        };
-
-        for (String sender : senders) {
-            assertIsGroup(sender);
+    public void when_a_title_contain_at_sign_then_is_group() {
+        for (String title : titles) {
+            this.title = title;
+            assertIsGroup(title);
         }
-
     }
 
-    private void assertIsGroup(String sender) {
-        Assert.assertTrue(String.format("sender:%s", sender), isGroup(sender));
+    private void assertIsGroup(String title) {
+        Assert.assertTrue(String.format("sender:%s", title), isGroup());
     }
 
-    private boolean isGroup(String sender) {
-        return sender.contains("@");
+    @Test
+    public void when_a_title_is_group_then_sender_and_group_extract_correctly() {
+        for (int i = 0; i < titles.length; i++) {
+            title = titles[i];
+            if (isGroup()) {
+                String sender = getSender();
+                String group = getGroup();
+                Assert.assertEquals(senders[i], sender);
+                Assert.assertEquals(groups[i], group);
+            }
+        }
+    }
+
+    private String getGroup() {
+        return title.split("@")[1].trim();
+    }
+
+    private String getSender() {
+        return title.split("@")[0].trim();
+    }
+
+    private boolean isGroup() {
+        return title.contains("@") && title.split("@").length == 2;
     }
 
 }
