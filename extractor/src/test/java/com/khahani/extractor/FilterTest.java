4909c0a059e3f51a9c3cae18a53e1b7fcc93eb7f
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-04 21:50:08 +0330

    add json library for testing purpose.

--- extractor/src/test/java/com/khahani/extractor/FilterTest.java
+++ extractor/src/test/java/com/khahani/extractor/FilterTest.java
@@ -1,5 +1,9 @@
 package com.khahani.extractor;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FilterTest {
@@ -7,7 +11,20 @@ public class FilterTest {
     private final String json = "[{\"lan\":\"en\",\"invalidSenders\":[\"WhatsApp\",\"null\"],\"regexSenderValidator\":[\"([2-9]|[1-9][0-9]|[1-9][0-9][0-9]) missed voice calls\",\"([2-9]|[1-9][0-9]|[1-9][0-9][0-9]) missed video calls\",\"\\\\(messages ([0-9]|[0-9][0-9]|[0-9][0-9][0-9])\\\\)\"],\"invalidMessages\":[\"null\",\"Checking for new messages\",\"Incoming voice call\",\"Incoming video call\",\"Missed voice call\",\"Missed video call\",\"\uD83D\uDCF7 Photo\",\"Calling…\",\"Ringing…\",\"Ongoing voice call\",\"Ongoing video call\",\"\uD83D\uDCF9 Incoming video call\"],\"invalidMessagesRegexRequired\":[\"2 new messages\",\"2 missed voice calls\",\"3 missed calls\"]}]";
 
     @Test
-    public void nothing() {
+    public void extract_language_from_json_test() {
 
+        String language = "";
+
+        try {
+            JSONArray root = new JSONArray(json);
+            for (int i = 0; i < root.length(); i++) {
+                JSONObject filter = root.getJSONObject(i);
+                language = filter.getString("lan");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals("en", language);
     }
 }
