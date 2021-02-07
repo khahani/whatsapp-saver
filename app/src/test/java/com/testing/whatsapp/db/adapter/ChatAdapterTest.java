90c01a277b874c22fc49a5ea967335b2650ece72
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-07 19:09:32 +0330

    create getChat method which convert a ReceivedMessage to a Chat

--- app/src/test/java/com/testing/whatsapp/db/adapter/ChatAdapterTest.java
+++ app/src/test/java/com/testing/whatsapp/db/adapter/ChatAdapterTest.java
@@ -1,5 +1,7 @@
 package com.testing.whatsapp.db.adapter;
 
+import androidx.annotation.NonNull;
+
 import com.testing.whatsapp.Model.Chat;
 import com.testing.whatsapp.db.ReceivedMessage;
 
@@ -25,18 +27,24 @@ public class ChatAdapterTest {
         receivedMessage.group = "c";
         receivedMessage.text = "Hi";
         receivedMessage.date = 1612711761281L;
-        String dateStr = "18:59";
+        String dateStr = "6:59 PM";
 
+        Chat chat = getChat(receivedMessage);
+
+        Assert.assertEquals(receivedMessage.sender, chat.getName());
+        Assert.assertEquals(receivedMessage.group, chat.getGroup());
+        Assert.assertEquals(dateStr, chat.getLastMessageTime());
+        Assert.assertEquals(receivedMessage.text, chat.getLastMessage());
+    }
+
+    @NonNull
+    private Chat getChat(ReceivedMessage receivedMessage) {
         Chat chat = new Chat();
         chat.setName(receivedMessage.sender);
         chat.setLastMessage(receivedMessage.text);
         chat.setGroup(receivedMessage.group);
         DateFormat df = DateFormat.getTimeInstance(DateFormat.SHORT);
         chat.setLastMessageTime(df.format(receivedMessage.date));
-
-        Assert.assertEquals(receivedMessage.sender, chat.getName());
-        Assert.assertEquals(receivedMessage.group, chat.getGroup());
-        Assert.assertEquals(dateStr, chat.getLastMessageTime());
-        Assert.assertEquals(receivedMessage.text, chat.getLastMessage());
+        return chat;
     }
 }
