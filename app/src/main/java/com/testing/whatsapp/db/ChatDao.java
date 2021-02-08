dbe0ea461099ca34e1e019ef7ecf860d57835a63
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-08 05:54:52 +0330

    create insert and getAll for ChatDao

--- app/src/main/java/com/testing/whatsapp/db/ChatDao.java
+++ app/src/main/java/com/testing/whatsapp/db/ChatDao.java
@@ -1,4 +1,17 @@
 package com.testing.whatsapp.db;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
 public interface ChatDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Chat chat);
+
+    @Query("Select * from chat order by date desc")
+    LiveData<List<Chat>> getAll();
 }
