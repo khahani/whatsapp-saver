bd4af0c3eeddb7c61173bff879f713fe8e9255e3
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-08 06:19:40 +0330

    make the class Runnable

--- app/src/main/java/com/testing/whatsapp/db/utils/ChatToDbMediator.java
+++ app/src/main/java/com/testing/whatsapp/db/utils/ChatToDbMediator.java
@@ -3,15 +3,20 @@ package com.testing.whatsapp.db.utils;
 import com.testing.whatsapp.db.Db;
 import com.testing.whatsapp.db.ReceivedMessage;
 
-public class ChatToDbMediator {
+public class ChatToDbMediator implements Runnable {
     protected final Db db;
+    private ReceivedMessage receivedMessage;
 
     public ChatToDbMediator(Db db) {
         this.db = db;
     }
 
-
     public void setChat(ReceivedMessage receivedMessage) {
+        this.receivedMessage = receivedMessage;
+    }
+
+    @Override
+    public void run() {
 
     }
 }
