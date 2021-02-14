bc3f1d36692ea350bf53b9336a08dcc35a1a047e
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-14 23:54:24 +0330

    update InterstitialCreator to return an AppodealInterstitial instance.

--- app/src/main/java/com/testing/whatsapp/creator/firebase/InterstitialCreator.java
+++ app/src/main/java/com/testing/whatsapp/creator/firebase/InterstitialCreator.java
@@ -2,9 +2,9 @@ package com.testing.whatsapp.creator.firebase;
 
 import android.app.Activity;
 
+import com.khahani.appodeal.AppodealInterstitial;
 import com.khahani.usecase_firebase.Creator;
 import com.khahani.usecase_firebase.admob.Interstitial;
-import com.testing.firebase.admob.InterstitialImpl;
 
 public class InterstitialCreator extends Creator<Interstitial> {
     private final Activity activity;
@@ -20,7 +20,8 @@ public class InterstitialCreator extends Creator<Interstitial> {
     @Override
     public Interstitial factoryMethod() {
 //        return new Nullnterstitial();
-        interstitial = new InterstitialImpl(activity, () -> interstitial.show(), realInterstitialId);
+        // interstitial = new InterstitialImpl(activity, () -> interstitial.show(), realInterstitialId);
+        interstitial = new AppodealInterstitial(activity);
         return interstitial;
     }
 }
