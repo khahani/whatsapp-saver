0e54f355cf7229fb20797ba76d1f4a23f466e590
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-15 15:25:50 +0330

    run initializer in AppodealBanner

--- appodeal/src/main/java/com/khahani/appodeal/AppodealBanner.java
+++ appodeal/src/main/java/com/khahani/appodeal/AppodealBanner.java
@@ -8,13 +8,16 @@ import com.khahani.usecase_firebase.admob.Banner;
 public class AppodealBanner extends Banner {
 
     private final Activity activity;
+    private final AppodealInitializer initializer;
 
     public AppodealBanner(Activity activity) {
         this.activity = activity;
+        this.initializer = new AppodealInitializer(activity);
     }
 
     @Override
     public void run() {
+        initializer.run();
         Appodeal.show(activity, Appodeal.BANNER_BOTTOM);
     }
 
