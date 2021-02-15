5a6153d8399d0ab06dc0123d580a932c9f94076e
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-15 15:23:42 +0330

    Add AppodealInitializer and move the initialization code from AppodealBanner to it.

--- appodeal/src/main/java/com/khahani/appodeal/AppodealBanner.java
+++ appodeal/src/main/java/com/khahani/appodeal/AppodealBanner.java
@@ -3,8 +3,6 @@ package com.khahani.appodeal;
 import android.app.Activity;
 
 import com.appodeal.ads.Appodeal;
-import com.explorestack.consent.Consent;
-import com.explorestack.consent.ConsentManager;
 import com.khahani.usecase_firebase.admob.Banner;
 
 public class AppodealBanner extends Banner {
@@ -17,11 +15,10 @@ public class AppodealBanner extends Banner {
 
     @Override
     public void run() {
-
-        Consent consent = ConsentManager.getInstance(activity).getConsent();
-        int adTypes = Appodeal.INTERSTITIAL | Appodeal.BANNER;
-        Appodeal.initialize(activity, activity.getString(R.string.appodeal_key), adTypes, consent);
-        Appodeal.setTesting(BuildConfig.DEBUG);
         Appodeal.show(activity, Appodeal.BANNER_BOTTOM);
+    }
+
+    private void getBannerView() {
+        Appodeal.getBannerView(activity);
     }
 }
---
+++ appodeal/src/main/java/com/khahani/appodeal/AppodealInitializer.java
@@ -0,0 +1,25 @@
+package com.khahani.appodeal;
+
+import android.app.Activity;
+
+import com.appodeal.ads.Appodeal;
+import com.explorestack.consent.Consent;
+import com.explorestack.consent.ConsentManager;
+
+public class AppodealInitializer implements Runnable {
+    private final Activity activity;
+
+    public AppodealInitializer(Activity activity) {
+        this.activity = activity;
+    }
+
+
+    @Override
+    public void run() {
+        Consent consent = ConsentManager.getInstance(activity).getConsent();
+        int adTypes = Appodeal.INTERSTITIAL | Appodeal.BANNER;
+        Appodeal.initialize(activity, activity.getString(R.string.appodeal_key), adTypes, consent);
+        Appodeal.setTesting(BuildConfig.DEBUG);
+
+    }
+}
