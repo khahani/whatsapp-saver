5f9012491d8a672592b79dd4d26cad63f03282a4
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-16 00:40:26 +0330

    clear MainActivity and extract some functionality to new classes

---
+++ app/src/main/java/com/testing/whatsapp/dialog/NotificationServiceAlertDialog.java
@@ -0,0 +1,113 @@
+package com.testing.whatsapp.dialog;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.khahani.usecase_firebase.analytic.click.AnalyticDialogClickListener;
+import com.khahani.usecase_firebase.analytic.click.AnalyticDismissListener;
+import com.testing.whatsapp.Activities.BaseActivity;
+import com.testing.whatsapp.Activities.BaseActivityValidator;
+import com.testing.whatsapp.R;
+
+public class NotificationServiceAlertDialog implements Runnable {
+    private static final String ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
+    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    private final Activity activity;
+
+    public NotificationServiceAlertDialog(Activity activity) {
+        new BaseActivityValidator(activity).run();
+        this.activity = activity;
+    }
+
+    private void setUpNotificationService() {
+        if (!isNotificationServiceEnabled()) {
+            NotificationServiceAlertDialog notificationServiceAlertDialog = new NotificationServiceAlertDialog(activity);
+            AlertDialog enableNotificationListenerAlertDialog = notificationServiceAlertDialog.build();
+            enableNotificationListenerAlertDialog.show();
+        }
+    }
+
+    private AlertDialog build() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+        alertDialogBuilder.setTitle(R.string.notification_listener_service);
+        alertDialogBuilder.setMessage(R.string.notification_listener_service_explanation);
+        alertDialogBuilder.setPositiveButton(R.string.yes, new AnalyticDialogClickListener(((BaseActivity) activity).getAnalytic()) {
+            @Override
+            protected String getName() {
+                return "Yes";
+            }
+
+            @Override
+            protected String getId() {
+                return "grant_access_to_notification_service";
+            }
+
+            @Override
+            protected void click(DialogInterface dialog, int which) {
+                activity.startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+            }
+        });
+        alertDialogBuilder.setNegativeButton(R.string.no, new AnalyticDialogClickListener(((BaseActivity) activity).getAnalytic()) {
+            @Override
+            protected String getName() {
+                return "No";
+            }
+
+            @Override
+            protected String getId() {
+                return activity.getString(R.string.revoke_access_to_notification_service);
+            }
+
+            @Override
+            protected void click(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).setOnDismissListener(new AnalyticDismissListener(((BaseActivity) activity).getAnalytic()) {
+
+            @Override
+            protected String getName() {
+                return "Dismiss";
+            }
+
+            @Override
+            protected String getId() {
+                return activity.getString(R.string.revoke_access_to_notification_service);
+            }
+
+            @Override
+            protected void dismiss(DialogInterface dialog) {
+                dialog.dismiss();
+            }
+        });
+        return (alertDialogBuilder.create());
+    }
+
+    private boolean isNotificationServiceEnabled() {
+        String pkgName = activity.getPackageName();
+        final String flat = Settings.Secure.getString(activity.getContentResolver(),
+                ENABLED_NOTIFICATION_LISTENERS);
+        if (!TextUtils.isEmpty(flat)) {
+            final String[] names = flat.split(":");
+            for (String name : names) {
+                final ComponentName cn = ComponentName.unflattenFromString(name);
+                if (cn != null) {
+                    if (TextUtils.equals(pkgName, cn.getPackageName())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        setUpNotificationService();
+    }
+}
