65272fe65d2b2dc89527c1768001e146ff8708e3
Mohammadreza khahani <mr.khahani@gmail.com>
2021-02-21 02:13:15 +0330

    create PerformanceAndPerformancableMediator

--- app/src/main/java/com/whatsappear/TheNotificationListener.java
+++ app/src/main/java/com/whatsappear/TheNotificationListener.java
@@ -5,8 +5,8 @@ import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
-import com.khahani.usecase_firebase.performance.Performancable;
 import com.khahani.usecase_firebase.performance.Performance;
+import com.khahani.usecase_firebase.performance.PerformanceAndPerformancableMediator;
 import com.whatsappear.creator.firebase.PerformanceCreator;
 import com.whatsappear.db.Db;
 import com.whatsappear.db.utils.ChatToDbMediator;
@@ -17,9 +17,11 @@ import com.whatsappear.db.utils.NotificationToDbMediatorBase;
 public class TheNotificationListener extends NotificationListenerService {
 
     private final Performance performance;
+    private final PerformanceAndPerformancableMediator ppm;
 
     public TheNotificationListener() {
         performance = new PerformanceCreator().factoryMethod();
+        ppm = new PerformanceAndPerformancableMediator();
     }
 
     @Override
@@ -33,22 +35,21 @@ public class TheNotificationListener extends NotificationListenerService {
 
         if (notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
 
+            ppm.setPerformance(performance);
+
             NotificationToDbMediatorBase ndb = new NotificationToDbMediator(getApplicationContext(), sbn);
-            runWithPerformance(ndb);
+            ppm.setPerformancable(ndb);
+            ppm.run();
 
             if (ndb.inserted()) {
                 ChatToDbMediator ctd = new ChatToDbMediator(Db.getInstance(getApplicationContext()));
                 ctd.setChat(ndb.getReceivedMessage());
-                runWithPerformance(ctd);
+                ppm.setPerformancable(ctd);
+                ppm.run();
             }
         }
     }
 
-    private void runWithPerformance(Performancable ctd) {
-        performance.newTrace(ctd.getTrackerKey());
-        performance.setMustTraceObject(ctd);
-        performance.run();
-    }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
